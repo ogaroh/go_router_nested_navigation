fdcb0dc63be84a77360b851722bc144029bf2e26
ogaroh <ogaroerick1998@gmail.com>
2024-01-31 16:32:13 +0300

    fix: `UTF-8` warning in Powershell

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:nested_navigation_gorouter_example/core/router/router.dart'
